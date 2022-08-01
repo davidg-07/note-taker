f684c4332575671ebab55129db94d3218dcba69f
David Galvez <david.dmg17@gmail.com>
2022-07-31 21:37:42 -0700

    fixed linked to css and added save to notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
@@ -8,15 +8,35 @@ const port = 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname,'/public')))
+app.use(express.static('public'));
 
+
+app.get('/api/notes', (req, res) => {
+  const data = fs.readFileSync('db/db.json', 'utf8')
+  res.send(data);
+})
+
+app.post('/api/notes', (req, res) => {
+  // TODO: investigate why it is adding the note twice.
+  const data = fs.readFileSync('db/db.json', 'utf8')
+  const notes = JSON.parse(data)
+  console.log('req: ', req.body);
+  notes.push(req.body);
+  console.log('notes: ', notes);
+  fs.writeFileSync(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
+})
+
+
+app.delete('/api/notes', (req, res) => {
+  // TODO: 
+});
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(port, () => {
