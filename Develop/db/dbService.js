6934ec4a5776822742a2702b2e1a5d59aea3634c
David Galvez <david.dmg17@gmail.com>
2022-08-03 23:09:32 -0700

    fixed error with double post

--- Develop/db/dbService.js
+++ Develop/db/dbService.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
+
+const readFileSync = util.promisify(fs.readFile)
+const writeFileSync = util.promisify(fs.writeFile)
 
 function readFromDb() {
-    const data = fs.readFileSync('db/db.json', 'utf8')
+    const data = fs.readFileSync('db/db.json', 'utf8');
     return data
 }
 
 function writeToDb(note) {
-    const data = fs.readFileSync('db/db.json', 'utf8')
-    const notes = JSON.parse(data)
-    notes.push(note)
-
+    const data = fs.readFileSync('db/db.json', 'utf8');
+    const notes = JSON.parse(data);
+    notes.push(note);
+    const writingData = fs.writeFileSync('db/db.json', JSON.stringify(notes));
+    return notes
+    
 }
 
 module.exports = {writeToDb, readFromDb}
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const dbService = require('./db/dbService')
 
 const app = express();
 const port = 3000;
@@ -17,19 +18,36 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.post('/api/notes', (req, res) => {
+  const data = dbService.writeToDb(req.body);
+  res.json(data);
+  // .then(note => res.json(note))
+  // .catch(err => res.status(500).json(err))
+
   // TODO: investigate why it is adding the note twice.
-  const data = fs.readFileSync('db/db.json', 'utf8')
-  const notes = JSON.parse(data)
-  console.log('req: ', req.body);
-  notes.push(req.body);
-  console.log('notes: ', notes);
-  fs.writeFileSync(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
+  // const data = fs.readFileSync('db/db.json', 'utf8')
+  // const notes = JSON.parse(data)
+  // console.log('req: ', req.body);
+  // notes.push(req.body);
+  // console.log('notes: ', notes);
+  // fs.writeFileSync(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
 })
 
-
-app.delete('/api/notes', (req, res) => {
-  // TODO: 
-});
+// Delete notes once done with them
+// app.delete('/api/notes', (req, res) => {
+//   let jsonFilePath = path.join(__dirname, "/");
+//   for (let i = 0; < data.length; i++) {
+//     if (database[i].id == req.params.id) {
+//       database.splice(i, 1);
+//       break;
+//     }
+//   }
+//   // Write the bd.json file again
+//   fs.writeFileSync(jsonFilePath, JSON.stringify(data), function (err) {
+//     if(err) {
+//       return console.log("your note was deleted!");
+//     }
+//   })
+// });
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
